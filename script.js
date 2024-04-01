e3ee8e5b92aae734116f6672c7c002e635bf03bd
Mrityunjayyshukla <shuklamrityunjay60@gmail.com>
2024-04-02 00:56:29 +0530

    completed intro

--- script.js
+++ script.js
@@ -33,11 +33,11 @@ function navigateToYoutube(){
     window.location.href = "https://youtube.com";
 }
 
-//Clock Effect
+//Time
 setInterval(setTime, 1000);
 setTime();
 function setTime() {
     var dt = new Date();
-    time = dt.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+    time = dt.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit'});
     document.getElementById('date-time').innerHTML = time;
 }
